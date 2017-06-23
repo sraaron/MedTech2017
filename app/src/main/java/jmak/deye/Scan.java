bfc483230420d3dc05530e06db5832396fa32d42
Jonathan Mak <jmak@stanford.edu>
2017-06-23 12:29:22 +0800

    Changed some text in the tabs

--- app/src/main/java/jmak/deye/Scan.java
+++ app/src/main/java/jmak/deye/Scan.java
@@ -6,25 +6,30 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
 
 public class Scan extends AppCompatActivity {
 
     private TextView mTextMessage;
 
+    public void buttonOnClick(View v) {
+        Button button1 = (Button) v;
+        button1.setText("Scan");
+
+    }
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText(R.string.scan_description);
+//                    scan();
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText(R.string.title_history);
                     return true;
             }
             return false;
